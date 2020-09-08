6a7e0b072967d9430cef3317d3faa1b0e00c8460
pcotte <pcotte@advestis.com>
2020-09-08 10:12:36 +0200

    fix: usr: Pytest are running (but failing)

--- tests/unit/test_ntf.py
+++ tests/unit/test_ntf.py
@@ -3,12 +3,16 @@ from nmtf import NTF
 import pandas as pd
 import numpy as np
 import json
+from pathlib import Path
+
+DATA_PATH = Path(__file__).parent.parent / "data"
 
 
 def test():
-    df = pd.read_csv('../data/data_Tensor.csv')
+    print(DATA_PATH)
+    df = pd.read_csv(DATA_PATH / "data_ntf.csv")
     expected_estimator = {}
-    with open("../data/result.json", "r") as ifile:
+    with open(DATA_PATH / "expected_result_ntf.json", "r") as ifile:
         decoded_array = json.load(ifile)
         for key in decoded_array:
             expected_estimator[key] = np.asarray(decoded_array[key])
@@ -18,4 +22,10 @@ def test():
     estimator = my_nt_fmodel.fit_transform(m0, n_blocks, sparsity=.8, n_bootstrap=10)
     estimator = my_nt_fmodel.predict(estimator)
     for key in estimator:
-        assert estimator[key] == expected_estimator[key]
+        key_exp = key
+        if key not in expected_estimator:
+            key_exp = key.upper()
+        print("")
+        print(f"Estimator[{key}]:{estimator[key]}")
+        print(f"Expected:{expected_estimator[key_exp]}")
+        assert (estimator[key] == expected_estimator[key_exp]).all()
