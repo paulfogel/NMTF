ded457603b8a8f8c37b5555b68ee249bf722db43
pcotte <pcotte@advestis.com>
2020-09-10 09:37:05 +0200

    chg: usr: more verbose PyTests

--- tests/unit/test_ntf.py
+++ tests/unit/test_ntf.py
@@ -3,13 +3,13 @@ from nmtf import NTF
 import pandas as pd
 import numpy as np
 import json
+import sys
 from pathlib import Path
 
 DATA_PATH = Path(__file__).parent.parent / "data"
 
 
 def test():
-    print(DATA_PATH)
     df = pd.read_csv(DATA_PATH / "data_ntf.csv")
     expected_estimator = {}
     with open(DATA_PATH / "expected_result_ntf.json", "r") as ifile:
@@ -22,10 +22,21 @@ def test():
     estimator = my_nt_fmodel.fit_transform(m0, n_blocks, sparsity=.8, n_bootstrap=10)
     estimator = my_nt_fmodel.predict(estimator)
     for key in estimator:
+        print("")
+        print(f"Testing {key}...")
         key_exp = key
         if key not in expected_estimator:
             key_exp = key.upper()
-        print("")
-        print(f"Estimator[{key}]:{estimator[key]}")
-        print(f"Expected:{expected_estimator[key_exp]}")
-        assert (estimator[key] == expected_estimator[key_exp]).all()
+        if isinstance(estimator[key], np.ndarray):
+            try:
+                np.testing.assert_array_almost_equal(estimator[key], expected_estimator[key_exp])
+            except AssertionError as e:
+                np.set_printoptions(threshold=sys.maxsize)
+                print("")
+                print(f"Estimator[{key}]:{estimator[key]}")
+                print(f"Expected:{expected_estimator[key_exp]}")
+                print("Differences:", estimator[key] - expected_estimator[key_exp])
+                raise e
+        else:
+            assert estimator[key] == expected_estimator[key_exp]
+        print("...ok")
